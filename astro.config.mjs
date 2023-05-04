78ea217a41a01d7a9404bd864a3c9d3652f91b71
Vladyslav Tkachenko <vlad@wtf.lv>
2023-05-04 13:02:46 +0300

    adding site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import image from "@astrojs/image";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+
 export default defineConfig({
+  site: "https://turn-digital.github.io",
+  // base: "schoolio-astro/",
   integrations: [
     astroI18next(),
     image({
